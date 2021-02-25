47eeb6ac4c9b9cf3da5f436aa8dbc6eb9eb0c192
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-02-25 04:06:31 -0600

    Update temperatures1.2.c
    I helped myself a little bit of Antonio Cortés' repository in the define part bc it didn't work well but now it does!!!

--- temperatures1.2.c
+++ temperatures1.2.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 
-/*print the Fahrenheit-Celius table
-      for fahr= 0,20,...,300*/
-main()
-{
-	int fahr, celsius;
-	int lower, upper, step;
+#define lower 0
+#define upper 300
+#define step 20
+
+int main (){
+	int fahr; 
+	int celsius;
 	
-	lower=0;  /*lower limit of the table of temperatures*/
-	upper=300; /*upper limit*/
-	step=20; /*increment size*/
-	
-	main()
-	;fahr=lower;
-	while(fahr<=upper){
-		celsius=5*(fahr-32)/9;
-		printf("%d\t%d\n", fahr, celsius);
-		fahr= fahr + step;
+	fahr=lower;
+    printf("Fahrenheit\tCelsius\n")
+    ;while (fahr <= upper){
+	     
+		celsius= 5*(fahr-32) / 9;
+		printf("%3d\t\t%3d\n", fahr, celsius);
+		fahr+=step;
 	}
-}
+
+   }
