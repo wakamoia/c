fb72195bcc43223cab5690cef263ddf7871f1a79
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-03-03 19:58:18 -0600

    Create 1_5_3exercise1_10.c

---
+++ examples_c1/1/1_5_3exercise1_10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int c;
+	while((c=getchar())!=EOF)
+	{
+		if(c=='\t')
+  {
+		putchar('\\');
+		putchar('t');
+	}	
+	else	if (c=='\b')
+  {
+		putchar('\\');
+		putchar('b');
+	}
+		else if (c=='\\')
+  {
+		putchar('\\');
+		putchar('\\');
+	}
+		else 
+		(putchar(c));
+	}	
+}
