e86aba38611c832453b11701fb112018e2a9a9b3
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-04-15 02:28:10 -0500

    Create library.h

---
+++ homework/homework4/library.h
@@ -0,0 +1,21 @@
+float sum(float num1, float num2)
+{
+	return num1+num2;
+}
+float subtract(float num1, float num2)
+{
+	return num1-num2;
+}
+float divide(float dividend, float divisor)
+{
+	return num1/num2;
+}
+float residue(float dividend, float divisor)
+{
+	return dividend%divisor;
+}
+
+void push(double);
+double pop(void);
+int getop(char []);
+int getch(void);
