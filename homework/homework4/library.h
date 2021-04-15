f7a72d48d81041df6ffb22f841a3c5a56e8c3778
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-04-15 11:36:42 -0500

    Update library.h

--- homework/homework4/library.h
+++ homework/homework4/library.h
@@ -1,21 +1,10 @@
-float sum(float num1, float num2)
-{
-	return num1+num2;
-}
-float subtract(float num1, float num2)
-{
-	return num1-num2;
-}
-float divide(float dividend, float divisor)
-{
-	return num1/num2;
-}
-float residue(float dividend, float divisor)
-{
-	return dividend%divisor;
-}
-
 void push(double);
 double pop(void);
 int getop(char []);
 int getch(void);
+void ungetch(int);
+
+int getlet(void);
+void ungetlet(int);
+void pushlet(char);
+char poplet(void);
