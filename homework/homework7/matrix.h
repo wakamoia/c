4ccaec3fc75a49d4034549fa6b9f27fec76e1b67
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-05-06 02:25:26 -0500

    Create matrix.h

---
+++ homework/homework7/matrix.h
@@ -0,0 +1,10 @@
+#define MAX 50
+#define USUARIOS 12
+int* registered = 0; 
+int** matrix; 
+int Texit=0; 
+int count();
+void registered();
+void hiorbye(int, int);
+void header();
+void horario();
