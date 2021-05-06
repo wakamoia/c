e83e5c3b8a92d26f3b73f4182742839d844e629b
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-05-06 15:25:16 -0500

    Update matrix.h

--- homework/homework7/matrix.h
+++ homework/homework7/matrix.h
@@ -1,10 +1,10 @@
 #define MAX 50
 #define USUARIOS 12
-int* registered = 0; 
+int* check = 0; 
 int** matrix; 
 int Texit=0; 
 int count();
-void registered();
+void check();
 void hiorbye(int, int);
 void header();
 void horario();
