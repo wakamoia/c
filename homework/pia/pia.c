3460cac6bd730015564facee6add464c4cab2401
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-05-19 15:12:11 -0500

    Update pia.c

--- homework/pia/pia.c
+++ homework/pia/pia.c
@@ -24,26 +24,26 @@ int main(){
 int op = 2;
   while(op == 2) /* AQUI SE LE PREGUNTA AL USUARIO SI ESTA LISTO PARA COMENZAR CON EL TEST*/
      {
-	    printf("  estas list@ para comenzar el test?\nyes = 1\t\t\t\tno = 2\n");
+	    printf(" are you ready to start the test?\nyes = 1\t\t\t\tno = 2\n");
   	     scanf("%d",&op); 
 	     system("cls");
  		 	break; }
   while(op == 2){ /*si la rescuesta es no entonces se le repite la pregunta hasta que este listo*/
 		 system("cls");
  
-		printf("\t   ahora estas list@?\nyes = 1\t\t\t\tno = 2\n");
+		printf("\t       Are you now?\nyes = 1\t\t\t\tno = 2\n");
  	 	 scanf("%d",&op); 
 		 system("cls");
 	 }
 
 	int cant;
 	
-	printf("hola soy ms. doki hoy te ayudare para saber si eres un@ persona infectada de covid o si eres muy propens@ a contagiarte\n");
-	printf("introduce los datos que se te piden, edad, peso, enfermedades cronicas\n");
+	printf("hello there ms. doki and today im gonna help you to know if to find out if you are a covid infected or if you just have a flu\n");
+	printf("enter the data you are asked for such as age, weight, chronic diseases, these data are important for the final beedict\n");
 	
-		printf("\ninserte cuantos usuarios se realizaran el test\n");
+		printf("\ninsert how many users will take the test\n");
 		scanf("%i",&cant);
-	
+	system("cls");
 int i;
 
 for(i=0; i<cant; i++)
@@ -51,20 +51,28 @@ for(i=0; i<cant; i++)
 { fflush(stdin); /*AQUI SE RECAUDAN LOS DATOS DEL USUARIO*/
 				printf("user[%d] ~ your name: ", i);
 	  		gets(user[i].name);	
-	int age;
-			printf("user[%i] ~ age: ", i); /*SI EL USUARIO ESTA EN UN RANGO DE EDAD ELEVADO O MUY RIESGOSO, SE LE INFORMARA */
+
+			printf("user[%i]  age: ", i); /*SI EL USUARIO ESTA EN UN RANGO DE EDAD ELEVADO O MUY RIESGOSO, SE LE INFORMARA */
 			scanf("%i" ,&user[i].age); 
-	if(age > 40)	{
-		puts("poblacion de mayor riesgo, mantenerse alerta!!");
-	}
-	float weight;
-		printf("user[%i] ~ weight: ", i); /*SI EL USUARIO TIENE OBESIDAD O SOBRE PESO SE LE INFORMARA YA QUE ES UN FACTOR PARA CONTAGIO COVID*/
-			scanf("%f", &user[i].weight); 
-	if(weight > 85 ){
-		puts("sobrepeso/obesidad, propenso a covid");
+				int age = user[i].age;
+	if((age > 21) && (age < 50) ){
+		puts("!!!are the most infected");
 	}	
+	if(age > 51 )	{
+		puts("population at greatest risk, stay alert!!");
+	}
+	
+		printf("user[%i]  weight: ", i); /*SI EL USUARIO TIENE OBESIDAD O SOBRE PESO SE LE INFORMARA YA QUE ES UN FACTOR PARA CONTAGIO COVID*/
+			scanf("%f", &user[i].weight);
+			 float weight= user[i].weight;
+	if(weight > 85 ){
+		puts("overweight/obesity, prone to covid");
+	}	
+	if(weight > 110)	{
+		puts("obesity, very prone to covid");
+	}
 			int issue, count=1, n, m;
-		printf("\ncuantas de estas enfermedades tiene ud? indiquelo con numeros. ejemplo: 1234 o solo <1> en caso de no padecerlas\n->(2)diabetes,(3) obesidad, (4)asma, (5)edad adulta<-\n");		
+		printf("\nhow many of these diseases do you have?\nindicate it with numbers. example: 1234 or only <1> if you do not develop them \n>(2)diabetes,(3) obesity, (4)asthma, (5)adulthood<-\n");		
 				printf("user[%d] ~ issues:", i); /*LAS ENFERMEDADES CRONICAS SON UN FACTOR MUY IMPORTANTE DE CONTAGIO COVID, ASI QUE ES IMPORTANTE TENER ESTOS DATOS*/
 				scanf("\n%d", &issue);
 			while(issue >4)
@@ -77,47 +85,48 @@ for(i=0; i<cant; i++)
 			
 			issue =m;
 			}
-			 printf("\n%d enfermedad(es)\n", count);
+			 printf("\n%d diseases\n", count);
 			 
 		int covis, cont=1, a, b;	
 			
-		printf("\n cuantos de estos sintomas presentas?");/*SE ENTISTAN SITNOMAS Y SE LE PIDE AL USUARIO QUE ESPECIFIQUE DE CUALES PADECE, DESPUES ESTOS SE CUENTAN*/
+		printf("\n how many of these symptoms do you present?");/*SE ENTISTAN SITNOMAS Y SE LE PIDE AL USUARIO QUE ESPECIFIQUE DE CUALES PADECE, DESPUES ESTOS SE CUENTAN*/
 		
-		printf("\n1-Fiebre o escalofrios");
-		printf("\n2-Congestion nasal/Tos");
-		printf("\n3-Dificultad para respirar  osea  sentir que le falta el aire");
-		printf("\n4-Fatiga");
-		printf("\n5-Dolores musculares y corporales");
-		printf("\n6-Dolor de cabeza");
-		printf("\n7-Perdida reciente del olfato o el gusto");
-		printf("\n8-Dolor de garganta");
-		printf("\n9- vomitos/ diarrea");
+		printf("\n1-Fever or chills");
+		printf("\n2-Nasal congestion or cough");
+		printf("\n3-Difficulty breathing, like you're short of breath");
+		printf("\n4-Fatigue");
+		printf("\n5-Muscle and body aches");
+		printf("\n6-Headache");
+		printf("\n7-Recent loss of smell or taste");
+		printf("\n8-Sore throat");
+		printf("\n9- Vomiting or diarrhea\n");
 		
-		scanf("\n%d ", &covis);
+		scanf("\n%I", &covis);
 			
 			while(covis >9)
 			{
-				b= issue/10;
+				b= covis/10;
 				
-				a= issue %10;
+				a= covis %10;
 			
-			cont = count +1;
+			cont = cont +1;
 			
 			covis =b;
 			}
-			 printf("\n%d enfermedad(es)", cont);}
-		int puntos;
-		printf("introduzca cuantos sintomas y enfermedades obtuvo al final, ejemplo: 6+9= 15 puntos\n");
-		scanf("%f",&puntos);
+			 printf("\n%i covid syntoms\n", cont);
+		}	
 		
-		if(puntos>9){
-			puts("lamento informarte que lo mas probable es que tengas covid:c\n\n \\\\\\\\COVID POSSITIVE//////");
-		}	 
-			else{
-				puts("FELICIDADES NO TIENES COVID! o bueno, eso parece...\n\n \\\\\\\\COVID NEGATIVE//////");
-			} 
-
+		printf("\nintroduzca cuantos sintomas y enfermedades obtuvo al final, ejemplo: 6+9= 15 puntos ->");
+		int puntos;
+		scanf("%i",&puntos); 
+		for(i=0; i<user; i++){
+		
+		if(puntos>=9){
+		printf("user[%i]~ %s according to my calculations and thanks to the data entered, you have covid, you should go to a doctor\n\n \\\\\\\\COVID POSSITIVE//////",i , user[i].name);
+		}
+		else{
+				printf("FELICIDADES USER[%i]~ %s NO TIENES COVID! o bueno, eso parece...\n\n \\\\\\\\COVID NEGATIVE//////",i , user[i].name);
+			} break;
+}
 	return 0;
-
-	
 }
