db75299f293197c70ce62905a925361b29fba216
wakamoia <78566399+wakamoia@users.noreply.github.com>
2021-05-19 21:45:13 -0500

    Add files via upload

---
+++ homework/pia/function.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void resultado(int);
+
+struct USER{ //HERE IS A STRUCTURE FOR ALL THE USERS THAT COULD USE THIS TEST/
+
+	char name[20];
+	int age;
+	float weight;
+	int issue;
+	int covis;
+	float height;
+} user;
